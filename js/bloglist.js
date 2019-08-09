bf3669c17bf255c4df4097b9335bacf575300eb0
rishabhjain05b <rishabhjain_05b@yahoo.in>
2019-08-09 20:35:12 +0530

    5th commit

--- js/bloglist.js
+++ js/bloglist.js
@@ -3,6 +3,11 @@ var binmodal = document.getElementById("binmodal");
 
 // Get the button that opens the modal
 var binbtn = document.getElementById("bin");
+var binbtn2 = document.getElementById("bin2");
+var binbtn3 = document.getElementById("bin3");
+var binbtn4 = document.getElementById("bin4");
+var binbtn5 = document.getElementById("bin5");
+
 
 // Get the <span> element that closes the modal
 var binspan = document.getElementsByClassName("binclose")[0];
@@ -11,7 +16,18 @@ var binspan = document.getElementsByClassName("binclose")[0];
 binbtn.onclick = function() {
   binmodal.style.display = "block";
 }
-
+binbtn2.onclick = function() {
+    binmodal.style.display = "block";
+  }
+  binbtn3.onclick = function() {
+    binmodal.style.display = "block";
+  }
+  binbtn4.onclick = function() {
+    binmodal.style.display = "block";
+  }
+  binbtn5.onclick = function() {
+    binmodal.style.display = "block";
+  }
 // When the user clicks on <span> (x), close the modal
 binspan.onclick = function() {
   binmodal.style.display = "none";
