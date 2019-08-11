578ef565758580ea704a0f19705f357713bf8789
rishabhjain05b <rishabhjain_05b@yahoo.in>
2019-08-11 23:42:22 +0530

    8th commit

--- js/index.js
+++ js/index.js
@@ -1,30 +1,17 @@
-// Get the modal
-var modal3 = document.getElementById("postsmodal");
-
-// Get the button that opens the modal
-var btn3 = document.getElementById("createposts");
-
-// Get the <span> element that closes the modal
-var span3 = document.getElementsByClassName("close3")[0];
-
-// When the user clicks on the button, open the modal 
-btn3.onclick = function() {
-  modal3.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span3.onclick = function() {
-  modal3.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal3) {
-    modal3.style.display = "none";
-  }
-}
-
-var allposts = document.getElementById("allposts");
+//  Function for opening bloglist page on All Posts button click
+var allposts = document.getElementById("allPosts");
 allposts.onclick = function(){
  window.location = "./html/bloglist.html";
+}
+
+// Function to display create Post Modal
+var btn2 = document.getElementById("createPosts");
+btn2.onclick = function(){
+createPostModal.style.display = "block";
+}
+
+// Create post modal X functionality
+var span2 = document.getElementById("close1");
+span2.onclick = function(){
+createPostModal.style.display="none";
 }
